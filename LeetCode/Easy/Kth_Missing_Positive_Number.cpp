01ad80131797401c50de2e4e3896ee8f09c7e9e9
Vignesh Shekar <vignesh.shekar10@gmail.com>
2026-08-17 23:09:29 +0530

    Create Kth_Missing_Positive_Number.cpp

---
+++ LeetCode/Easy/Kth_Missing_Positive_Number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int start = 0 , end = arr.size()-1;
+        while (start <= end){
+            int mid = start + (end - start)/2;
+            if (arr[mid] - mid - 1 >= k) end = mid - 1;
+            else start = mid + 1;
+            }
+        return start + k;
+    }
+};
