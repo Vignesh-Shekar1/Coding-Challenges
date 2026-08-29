cb0f5b3ebee0b115da225f65ee6a08c7bfb714cc
Vignesh Shekar <vignesh.shekar10@gmail.com>
2026-08-29 21:09:49 +0530

    Create Buy_Two_Chocolates.cpp

---
+++ LeetCode/Easy/Buy_Two_Chocolates.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        int min1, min2;
+        if (prices[0] < prices[1]) min1 = prices[0], min2 = prices[1];
+        else min1 = prices[1], min2 = prices[0];
+        for (int i = 2; i < prices.size(); ++i){
+            if (prices[i] < min1){
+                min2 = min1;
+                min1 = prices[i];
+            }
+            else if (prices[i] < min2) min2 = prices[i];
+        }
+        if (min1 + min2 > money) return money;
+        return money - min1 - min2;
+    }
+};
